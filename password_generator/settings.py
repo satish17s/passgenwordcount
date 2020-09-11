3f75cf19b12ed8c36ac9a83150e290ef38e5b9c3
SATISH SHARMA <satishsharma.cse@gmail.com>
2020-09-11 16:03:45 +0530

    Update settings.py

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nacp9(db&8w77=d36xu&r379zrd4-gy57ty-yn0=@mf-%r#1k)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','*','localhost',]
 
